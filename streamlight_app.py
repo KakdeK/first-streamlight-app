8a3677ba06b314db87ae456ed35196d71c223a53
Kapil Kakde <107403630+KakdeK@users.noreply.github.com>
2022-06-13 16:39:42 +0530

    Update streamlight_app.py

--- streamlight_app.py
+++ streamlight_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('My parents new healthy dinner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Idli')
+streamlit.text('Dosa')
+streamlit.text('Samosa')
+
