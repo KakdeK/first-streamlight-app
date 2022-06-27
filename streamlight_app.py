0c809b36b68e7540b97a16bb5ea5c299070093fe
Kapil Kakde <107403630+KakdeK@users.noreply.github.com>
2022-06-27 11:27:52 +0530

    Update streamlight_app.py

--- streamlight_app.py
+++ streamlight_app.py
@@ -6,3 +6,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣Idli')
 streamlit.text('🥗Dosa')
 streamlit.text('🍞Samosa')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
